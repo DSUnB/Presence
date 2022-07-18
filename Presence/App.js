75f9b9030dbe9f6cabebeb484260d09529df3ed8
Leandro Almeida <leo.leandro.ars@gmail.com>
2022-07-18 19:53:25 -0300

    Estudos de componentização e estilização com o Expo

--- Presence/App.js
+++ Presence/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Login from './Pages/Login/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>sflkshdfkjhsdgfl</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Login/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Presence/components/Input1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, TextInput } from 'react-native'
+
+export default function Input1() {
+  return (
+    <View>
+        <TextInput/>
+    </View>
+  )
+}
